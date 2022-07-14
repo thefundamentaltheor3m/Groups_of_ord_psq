5a78907e54d1c2e2fecfa20550730b82c27b43aa
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2022-07-14 16:41:21 +0400

    Slightly Concised Code

--- src/Fully_Working_Proof_V1.lean
+++ src/Fully_Working_Proof_V1.lean
@@ -15,6 +15,7 @@ open fintype
 open is_p_group
 open group
 
+
 /-- A group that is commutative can be turned into a comm_group
 (See https://github.com/leanprover-community/mathlib/pull/14960#issuecomment-1178880188)
 -/
@@ -23,21 +24,15 @@ instance {G} : can_lift (group G) (comm_group G) :=
   cond := λ g, by exactI (∀ x y : G, commute x y),
   prf := λ g hg, ⟨{mul_comm := hg, ..g}, by cases g; refl⟩ }
 
-
-/-- My lemma -/
-lemma group.center_eq_top_of_comm {H : Type*} [group H] (h : ∀ a b : H, a * b = b * a) : subgroup.center H = ⊤ :=
-begin
-  rw subgroup.eq_top_iff',
-  intros x y,
-  exact (h y x),
-end
-
+/-- A group whose center is ⊤ must be abelian. -/
 lemma group.of_comm_center_eq_top {H : Type*} [group H] (h : (subgroup.center H = ⊤)) : (∀ a b : H, a * b = b * a) :=
 begin
   rw subgroup.eq_top_iff' at h,
   intros x y,
   rw (h x),
 end
+
+/-- Basic Setup: `p` is a prime and `G` is a group of order `p` squared. -/
 
 variables (p : ℕ) [fact (nat.prime p)]
 variables (G : Type*) [fintype G] [group G]
@@ -46,7 +41,8 @@ def order_psq : Prop := card G = p^2
 
 namespace order_psq
 
-lemma has_ord_psq : order_psq p G → ∃ q : ℕ , card G = q^2 :=
+/-- The order of G is a perfect square. -/
+lemma has_ord_sq : order_psq p G → ∃ q : ℕ , card G = q^2 :=
 begin
   intro h1,
   use p,
@@ -58,31 +54,31 @@ section G_has_order_psq
 variables (hG : order_psq p G)
 include hG
 
+/-- G is a p-group. -/
 lemma p_group : is_p_group p G :=
 begin
   exact of_card hG,
 end
 
+/-- G has a nontrivial center Z(G). -/
 lemma psq_center_nontrivial [nontrivial G] : nontrivial (subgroup.center G) :=
 center_nontrivial (p_group p G hG)
 
 open_locale classical
 
+/-- G ⧸ Z(G) is a fintype. -/
 @[instance] def quotient_with_center_is_fintype : finite (G ⧸ subgroup.center G) :=
 infer_instance
 
+/-- |G ⧸ Z(G)| is not prime. -/
 lemma center_index_not_prime : ¬ nat.prime (fintype.card (G ⧸ subgroup.center G)) :=
 begin
   intro h1,
   haveI : fact(nat.prime (card (G ⧸ subgroup.center G))) := ⟨h1⟩,
   haveI := is_cyclic_of_prime_card (rfl : card (G ⧸ subgroup.center G) = _),
-  --letI := comm_group_of_cycle_center_quotient (quotient_group.mk' (subgroup.center G)) (by simp),
   have h2 := commutative_of_cyclic_center_quotient (quotient_group.mk' (subgroup.center G)) (by simp),
   unfreezingI { lift ‹group G› to comm_group G using h2 },
-  have h3 : subgroup.center G = ⊤,
-  {
-    exact comm_group.center_eq_top,  -- working
-  },
+  have h3 : subgroup.center G = ⊤, exact comm_group.center_eq_top,
   have h4 : card (G ⧸ subgroup.center G) = 1,
   {
     simp_rw [h3, ← nat.card_eq_fintype_card],
@@ -92,63 +88,44 @@ begin
   exact nat.not_prime_one h1,
 end
 
-open_locale cardinal
-
-#check subgroup.card_mul_index (subgroup.center G)
-
+/-- G is abelian. -/
 theorem order_psq_are_abelian [nontrivial G] : (∀ x1 x2 : G, x1 * x2 = x2 * x1) :=
 begin
+  -- We show it suffices to show that Z(G) = ⊤.
   apply group.of_comm_center_eq_top,
-  have h1 : (card (subgroup.center G) ∣ card (G)),
-  {
-    exact (subgroup.center G).card_subgroup_dvd_card,
-  },
-  have h2 : (card G = p^2),
-  {
-    exact hG,
-  },
-  rw h2 at h1,
-  rw nat.dvd_prime_pow at h1,
+  have h1 : (card (subgroup.center G) ∣ card (G)), exact (subgroup.center G).card_subgroup_dvd_card,
+  unfold order_psq at hG,
+  rw [hG, nat.dvd_prime_pow] at h1,
+  -- We know it suffices to show that |Z(G)| = |G| = p^2.
+  -- We can thus use Lagrange's Theorem and condition on |Z(G)|.
   rcases h1 with ⟨k, hk1, hk2⟩,
   swap,
   apply fact.out,
   interval_cases k,
-  -- # |Z(G)| = 1
+  -- We look at the case |Z(G)| = 1
   {
     simp at hk2,
     exfalso,
-    have h31 : nontrivial (subgroup.center G),
-    {
-      exact center_nontrivial (p_group p G hG),
-    },
+    have h31 : nontrivial (subgroup.center G), exact center_nontrivial (p_group p G hG),
     rw subgroup.nontrivial_iff_exists_ne_one at h31,
     rcases h31 with ⟨x, hx, hxx⟩,
-    have h32 : subgroup.center G = ⊥,
-    {
-      --have hcalc : card (subgroup.center G) ≤ 1, linarith,
-      --rw ← subgroup.card_le_one_iff_eq_bot,
-      --exact hcalc,
-      --simp_rw hk2,
-      rw [← subgroup.card_le_one_iff_eq_bot, hk2],
-    },
+    have h32 : subgroup.center G = ⊥, rw [← subgroup.card_le_one_iff_eq_bot, hk2],
     have hxx' : x = 1,
     {
-      --finish,
-      --rw h32 at hx,
       rw subgroup.eq_bot_iff_forall at h32,
       exact h32 x hx,
     },
     contradiction,
   },
-  -- # |Z(G)| = p
+  -- We look at the case |Z(G)| = p
   {
     exfalso,
     have h41 : p * (subgroup.center G).index = p^2,
     {
       simp at hk2,
       simp_rw ← nat.card_eq_fintype_card at hk2,
-      simp_rw ← nat.card_eq_fintype_card at h2,
-      rw [← h2, ← hk2],
+      simp_rw ← nat.card_eq_fintype_card at hG,
+      rw [← hG, ← hk2],
       exact subgroup.card_mul_index (subgroup.center G),
     },
     have h42 : (subgroup.center G).index = p,
@@ -175,8 +152,6 @@ begin
         exfalso,
         contradiction,
       },
-      --finish,  -- works
-      --sorry,  -- Compiles much faster than it would with `finish`
     },
     have h43 : nat.prime (card (G ⧸ subgroup.center G)),
     {
@@ -192,10 +167,10 @@ begin
     },
     contradiction,
   },
-  -- # |Z(G)| = p^2
+  -- We look at the case |Z(G)| = p^2
   {
     apply subgroup.eq_top_of_card_eq,
-    rw h2,
+    rw hG,
     exact hk2,
   },
 end
